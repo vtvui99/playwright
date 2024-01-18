1b9eb818b3ea4c35550281f11438dd9e474788f4
Vui Vo <vtvui99@gmail.com>
2024-01-18 10:24:31 +0700

    Update login testcases

--- tests/tadashboard/login-tests.spec.ts
+++ tests/tadashboard/login-tests.spec.ts
@@ -2,12 +2,13 @@ import { test, expect } from "@playwright/test";
 import { LoginPage } from "../../pages/login.page";
 import { DashboardMainPage } from "../../pages/dashboard-main.page";
 import message from "../../data/message.json";
+import users from "../../data/users.json"
 
 test.describe('Login Tests', () => {
     test('Verify that user can login specific repository successfully via Dashboard login page with correct credentials', async ({page}) => {
         const loginPage = new LoginPage(page);
         await loginPage.go();
-        await loginPage.login('administrator', '');
+        await loginPage.login(users.adminUser.username, users.adminUser.password);
 
         const mainPage = new DashboardMainPage(page);
         await mainPage.display();
@@ -20,6 +21,6 @@ test.describe('Login Tests', () => {
         page.on('dialog', async dialog => {
             expect.soft(dialog.message()).toBe(message.login_incorrectCredential);
         });
-        await loginPage.login('abc', 'abc');
+        await loginPage.login(users.invalidUser.username, users.invalidUser.password);
     })
 });
